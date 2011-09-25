773f4b5442671e0d88aa11f53019967f39def7e1
Tim Romberg <tim.jakobsson@gmail.com>
2011-09-25 22:03:07 +0200

    adding some things locally

--- main.rb
+++ main.rb
@@ -4,11 +4,19 @@ require 'instagram'
 require 'haml'
 require 'dalli'
 require 'yajl' 
+require 'logger'
+
 CACHE = Dalli::Client.new
 
 Instagram.configure do |config|
   config.client_id = 'bded657abe374e3da6ce4753286f6850'
-  config.client_secret = '5dd01feba30d4d4f9b99f57d0212c34a'
+  config.client_secret = '5dd01feba30d4d4f9b99f57d0212c34a'   
+end 
+
+def log_to_file
+  log = File.new("log/sinatra.log", "w")     
+  STDOUT.reopen(log)                          
+  STDERR.reopen(log) 
 end
 
 error 404 do
@@ -16,12 +24,13 @@ error 404 do
 end  
 
 def getPhotos
-  photos = Instagram.tag_recent_media('sun')
-  photos.find_all{ |photo| photo.filter =!nil}.map { |photo| photo.images.low_resolution.url} 
+  photos = Instagram.tag_recent_media('sun')  
+  puts photos
+  photos.find_all{ |photo| photo.filter =!nil}.map { |photo| photo.images.low_resolution.url}   
 end  
 
 
-get '/' do
-  @results = CACHE.fetch('sun', 900) { getPhotos() }
+get '/' do   
+  @results = CACHE.fetch('sun', 900) { getPhotos() }       
   haml :index
-end
+end   
